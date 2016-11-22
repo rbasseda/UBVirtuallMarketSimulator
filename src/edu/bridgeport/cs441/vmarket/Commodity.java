123ae8fe5b3c232d5309ebbb00d0b3f1b472a0e7
Reza Basseda <rbasseda@cs.stonybrook.edu>
2016-11-22 12:55:31 -0500

    We added this file based on our discussion

---
+++ src/edu/bridgeport/cs441/vmarket/Commodity.java
@@ -0,0 +1,40 @@
+/**
+ * Commodity.java
+ * 
+ * Copyright (C) The University of Bridgeport, 2016
+ * All rights reserved.
+ * 
+ * This software may be modified and distributed under the terms
+ * of the BSD license.  See the LICENSE file for details.
+ * 
+ * Created on: Nov 22, 2016
+ */
+package edu.bridgeport.cs441.vmarket;
+
+import edu.bridgeport.cs441.vmarket.parties.Person;
+
+/**
+ * The Commodity class is representing any item that can be traded in 
+ * the market.
+ * 
+ * The class has the following attributes:
+ * 
+ * 
+ * @author Reza Basseda
+ *
+ */
+public class Commodity implements Purchasable {
+
+	/**
+	 * @author 
+	 */
+	/* (non-Javadoc)
+	 * @see edu.bridgeport.cs441.vmarket.Purchasable#Purchased(int, edu.bridgeport.cs441.vmarket.parties.Person)
+	 */
+	@Override
+	public boolean Purchased(int price, Person reciever) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
