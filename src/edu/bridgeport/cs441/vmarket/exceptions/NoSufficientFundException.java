3cf85c8f48fa5bf6802ab7111008c6d935ff44be
Jia Fang <fangofjia@gmail.com>
2016-11-29 01:05:11 -0500

    Update NoSufficientFundException.java

--- src/edu/bridgeport/cs441/vmarket/exceptions/NoSufficientFundException.java
+++ src/edu/bridgeport/cs441/vmarket/exceptions/NoSufficientFundException.java
@@ -8,5 +8,12 @@ package edu.bridgeport.cs441.vmarket.exceptions;
  *
  */
 public class NoSufficientFundException extends Exception {
-
+private double amount;
+public NoSufficientFundException (double amount){
+  this.amount=amount;
+  System.out.println("Insufficient funds in the account of transaction. "+amount+" of currency is required");
+}
+  public double getAmount(){
+  return amount;
+  }
 }
